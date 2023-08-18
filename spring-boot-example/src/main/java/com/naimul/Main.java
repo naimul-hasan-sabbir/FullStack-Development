3a5a0380485a4c4f379689a0c347cf0fdc23ae9e
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-08-18 23:54:56 +0600

    updated

--- spring-boot-example/src/main/java/com/naimul/Main.java
+++ spring-boot-example/src/main/java/com/naimul/Main.java
@@ -5,6 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @SpringBootApplication
 @RestController
 public class Main {
@@ -17,5 +19,11 @@ public class Main {
         return new GreetResponse("Hello");
     }
 
-    record GreetResponse(String greet){}
+    record Person(String name){}
+
+    record GreetResponse(
+            String greet,
+            List<String> favProgrammingLanguages,
+            Person person
+    ){}
 }
